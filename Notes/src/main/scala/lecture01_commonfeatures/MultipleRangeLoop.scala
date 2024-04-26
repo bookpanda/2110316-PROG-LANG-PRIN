72dbb4c39e351a5427b5d6e79aaf452e3c66f986
Idhibhat Pankam <marginpankam@gmail.com>
2024-04-26 20:56:01 +0700

    fix: rename folder

--- Notes/src/main/scala/lecture01_commonfeatures/MultipleRangeLoop.scala
+++
@@ -1,9 +0,0 @@
-package lecture01_commonfeatures
-
-object MultipleRangeLoop {
-  def main(args: Array[String]): Unit = {
-    for(x <- 0.until(5); i <- 0 to 4) {
-      println(s"$x , $i")
-    }
-  }
-}
